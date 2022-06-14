42e6a5b272ea1555e41860f24bf6ef07d0a62306
Aurellya <aurellyagho25@gmail.com>
2022-06-14 16:51:42 +0800

    [update user management page - superadmin -> pass data from the clicked edit button to modal pop up window]

--- dhrecord/superadmin/js/index.js
+++ dhrecord/superadmin/js/index.js
@@ -1,1 +1,25 @@
-// js file
+// to pass information to modal when 'edit' button is clicked
+$(".edit-btn").click(function () {
+  // get the name
+  var $name = $(this)
+    .closest("tr") // Finds the closest row <tr>
+    .find("td:first")
+    .text(); // Retrieves the text within <td>
+  $("#inputName").val($name);
+
+  // get the address
+  var $address = $(this).closest("tr").find("td:eq(1)").text();
+  $("#inputAddress").val($address);
+
+  // get the NRIC
+  var $NRIC = $(this).closest("tr").find("td:eq(2)").text();
+  $("#inputNRIC").val($NRIC);
+
+  // get the contact no.
+  var $contactNo = $(this).closest("tr").find("td:eq(3)").text();
+  $("#inputContactNo").val($contactNo);
+
+  // get the email
+  var $email = $(this).closest("tr").find("td:eq(4)").text();
+  $("#inputEmail").val($email);
+});
