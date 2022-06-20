345c4c418ab3c90ad37eef06f9f1d4ddaa326bd5
Aurellya <aurellyagho25@gmail.com>
2022-06-20 14:25:43 +0800

    [revise superadmin 'registration management - business owner' page]

--- dhrecord/superadmin/js/index.js
+++ dhrecord/superadmin/js/index.js
@@ -236,7 +236,7 @@ function saveDetails() {
   $("#popupModal").modal("hide");
 }
 
-// REGISTRATION MANAGEMENT
+// REGISTRATION MANAGEMENT - PATIENT
 // sample data
 var data2 = [
   [
@@ -330,7 +330,7 @@ function searchName2() {
 
   // inject matched data
   let text = "";
-  for (i = 0; i < data.length; i++) {
+  for (i = 0; i < data2.length; i++) {
     if (data2[i][1].toUpperCase().indexOf(filter) > -1) {
       text +=
         '<tr><td scope="row">' +
@@ -375,4 +375,193 @@ function viewFullInfo(id) {
       $("#inputDrugAlle").val(data2[i][9]);
     }
   }
+}
+
+// REGISTRATION MANAGEMENT - BUSINESS OWNER
+// sample data
+var data3 = [
+  [
+    1,
+    "Y-Dental",
+    "5 Magazine Road #02-01, 059571, Singapore",
+    "+65 8950 4262",
+    "YDental@gmail.com",
+    "123456789012",
+    "C0019292",
+    "21/03/2006",
+    "Pediatric Dentistry",
+  ],
+  [
+    2,
+    "Dental Care",
+    "926 Yishun Central 1 01-177, 760926, Singapore",
+    "+65 8952 7201",
+    "DentalCare@gmail.com",
+    "234567890123",
+    "C0315261",
+    "08/08/2010",
+    "Periodontics, Prosthodontics",
+  ],
+  [
+    3,
+    "Go Dental",
+    "112 East Coast Road #B1-14 112 Katong, 428802, Singapore",
+    "+65 9786 5789",
+    "GoDental@gmail.com",
+    "345678901234",
+    "C2032352",
+    "17/09/2010",
+    "Oral surgery",
+  ],
+];
+
+// inject data to table in 'user management' page => onLoad
+function findData3() {
+  let text = "";
+  for (i = 0; i < data3.length; i++) {
+    text +=
+      '<tr><td scope="row">' +
+      data3[i][0] +
+      "</td><td>" +
+      data3[i][1] +
+      "</td><td>" +
+      data3[i][2] +
+      "</td><td>" +
+      data3[i][3] +
+      "</td><td>" +
+      data3[i][4] +
+      "</td><td>" +
+      data3[i][5] +
+      "</td><td>" +
+      data3[i][6] +
+      "</td><td>" +
+      data3[i][7] +
+      "</td><td>" +
+      data3[i][8] +
+      "</td>" +
+      '<td class="text-center"><button onclick="viewFullInfo3(' +
+      data3[i][0] +
+      ');" class="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#popupModal3">View</button></td>' +
+      '<td class="text-center"><button onclick="approveUser(' +
+      data3[i][0] +
+      ');" class="btn btn-sm btn-success">Approve</button></td>' +
+      "</tr>";
+  }
+
+  document.getElementById("data3").innerHTML += text;
+}
+
+// search name function
+function searchName3() {
+  // empty table
+  document.getElementById("data3").innerHTML = "";
+
+  // get user input
+  let input = document.getElementById("searchNameInput");
+  let filter = input.value.toUpperCase();
+
+  // inject matched data
+  let text = "";
+  for (i = 0; i < data3.length; i++) {
+    if (data3[i][1].toUpperCase().indexOf(filter) > -1) {
+      text +=
+        '<tr><td scope="row">' +
+        data3[i][0] +
+        "</td><td>" +
+        data3[i][1] +
+        "</td><td>" +
+        data3[i][2] +
+        "</td><td>" +
+        data3[i][3] +
+        "</td><td>" +
+        data3[i][4] +
+        "</td><td>" +
+        data3[i][5] +
+        "</td><td>" +
+        data3[i][6] +
+        "</td><td>" +
+        data3[i][7] +
+        "</td><td>" +
+        data3[i][8] +
+        "</td>" +
+        '<td class="text-center"><button onclick="viewFullInfo3(' +
+        data3[i][0] +
+        ');" class="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#popupModal3">View</button></td>' +
+        '<td class="text-center"><button onclick="approveUser(' +
+        data3[i][0] +
+        ');" class="btn btn-sm btn-success">Approve</button></td>' +
+        "</tr>";
+    }
+  }
+
+  document.getElementById("data3").innerHTML += text;
+}
+
+// function to pass information to modal when 'edit' button is clicked
+function viewFullInfo3(id) {
+  for (i = 0; i < data3.length; i++) {
+    if (data3[i][0] === id) {
+      $("#invisibleID").val(data3[i][0]);
+      $("#inputName").val(data3[i][1]);
+      $("#inputAddress").val(data3[i][2]);
+      $("#inputContactNo").val(data3[i][3]);
+      $("#inputEmail").val(data3[i][4]);
+      $("#inputRegistrationNo").val(data3[i][5]);
+      $("#inputLicenseNo").val(data3[i][6]);
+      $("#inputRegistrationDate").val(data3[i][7]);
+      $("#inputSpecialization").val(data3[i][8]);
+    }
+  }
+}
+
+// function to delete row when 'delete' button is clicked
+function approveUser(id) {
+  // empty table
+  document.getElementById("data3").innerHTML = "";
+
+  // remove deleted row from data
+  for (i = 0; i < data3.length; i++) {
+    if (data3[i][0] === id) {
+      data3.splice(i, 1);
+    }
+  }
+
+  // display updated table
+  let text = "";
+  for (i = 0; i < data3.length; i++) {
+    text +=
+      '<tr><td scope="row">' +
+      data3[i][0] +
+      "</td><td>" +
+      data3[i][1] +
+      "</td><td>" +
+      data3[i][2] +
+      "</td><td>" +
+      data3[i][3] +
+      "</td><td>" +
+      data3[i][4] +
+      "</td><td>" +
+      data3[i][5] +
+      "</td><td>" +
+      data3[i][6] +
+      "</td><td>" +
+      data3[i][7] +
+      "</td><td>" +
+      data3[i][8] +
+      "</td>" +
+      '<td class="text-center"><button onclick="viewFullInfo3(' +
+      data3[i][0] +
+      ');" class="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#popupModal3">View</button></td>' +
+      '<td class="text-center"><button onclick="approveUser(' +
+      data3[i][0] +
+      ');" class="btn btn-sm btn-success">Approve</button></td>' +
+      "</tr>";
+  }
+
+  document.getElementById("data3").innerHTML += text;
+  $("#alert_approval").css("display", "block");
+}
+
+function close_alert() {
+  $("#alert_approval").css("display", "none");
 }
