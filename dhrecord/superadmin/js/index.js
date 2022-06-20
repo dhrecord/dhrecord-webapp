52be54e961bbe0483af4295439dbf019c0278c25
Aurellya <aurellyagho25@gmail.com>
2022-06-20 13:30:33 +0800

    [revise superadmin 'registration management - patient account' page]

--- dhrecord/superadmin/js/index.js
+++ dhrecord/superadmin/js/index.js
@@ -1,3 +1,4 @@
+// USER MANAGEMENT
 // sample data
 var data = [
   [
@@ -233,4 +234,145 @@ function saveDetails() {
 
   document.getElementById("data").innerHTML += text;
   $("#popupModal").modal("hide");
+}
+
+// REGISTRATION MANAGEMENT
+// sample data
+var data2 = [
+  [
+    1,
+    "John Doe",
+    "5 Magazine Road #02-01, 059571, Singapore",
+    "S5219994H",
+    "+65 8950 4262",
+    "JohnDoe@gmail.com",
+    "kkO9Y7js",
+    "22/01/2019",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "Lorem ipsum dolor sit amet",
+  ],
+  [
+    2,
+    "Jane Doe",
+    "926 Yishun Central 1 01-177, 760926, Singapore",
+    "S6891261Z",
+    "+65 8952 7201",
+    "JaneDoe@gmail.com",
+    "l99Pas6d",
+    "18/08/2019",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "Lorem ipsum dolor sit amet",
+  ],
+  [
+    3,
+    "Nate",
+    "1 Coleman Street 02-30/31 The Adelphi, 179803, Singapore",
+    "S9168686D",
+    "+65 9786 5789",
+    "Nate22@gmail.com",
+    "9KAasY7w",
+    "07/09/2019",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "Lorem ipsum dolor sit amet",
+  ],
+  [
+    4,
+    "Jack",
+    "112 East Coast Road #B1-14 112 Katong, 428802, Singapore",
+    "S5397679D",
+    "+65 8951 7299",
+    "Jack11@gmail.com",
+    "9KAasY7w",
+    "27/10/2019",
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    "Lorem ipsum dolor sit amet",
+  ],
+];
+
+// inject data to table in 'user management' page => onLoad
+function findData2() {
+  let text = "";
+  for (i = 0; i < data2.length; i++) {
+    text +=
+      '<tr><td scope="row">' +
+      data2[i][0] +
+      "</td><td>" +
+      data2[i][1] +
+      "</td><td>" +
+      data2[i][2] +
+      "</td><td>" +
+      data2[i][3] +
+      "</td><td>" +
+      data2[i][4] +
+      "</td><td>" +
+      data2[i][5] +
+      "</td><td>" +
+      data2[i][6] +
+      "</td><td>" +
+      data2[i][7] +
+      "</td>" +
+      '<td class="text-center"><button onclick="viewFullInfo(' +
+      data2[i][0] +
+      ');" class="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#popupModal2">View</button></td></tr>';
+  }
+
+  document.getElementById("data2").innerHTML += text;
+}
+
+// search name function
+function searchName2() {
+  // empty table
+  document.getElementById("data2").innerHTML = "";
+
+  // get user input
+  let input = document.getElementById("searchNameInput");
+  let filter = input.value.toUpperCase();
+
+  // inject matched data
+  let text = "";
+  for (i = 0; i < data.length; i++) {
+    if (data2[i][1].toUpperCase().indexOf(filter) > -1) {
+      text +=
+        '<tr><td scope="row">' +
+        data2[i][0] +
+        "</td><td>" +
+        data2[i][1] +
+        "</td><td>" +
+        data2[i][2] +
+        "</td><td>" +
+        data2[i][3] +
+        "</td><td>" +
+        data2[i][4] +
+        "</td><td>" +
+        data2[i][5] +
+        "</td><td>" +
+        data2[i][6] +
+        "</td><td>" +
+        data2[i][7] +
+        "</td>" +
+        '<td class="text-center"><button onclick="viewFullInfo(' +
+        data2[i][0] +
+        ');" class="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#popupModal2">View</button></td></tr>';
+    }
+  }
+
+  document.getElementById("data2").innerHTML += text;
+}
+
+// function to pass information to modal when 'edit' button is clicked
+function viewFullInfo(id) {
+  for (i = 0; i < data2.length; i++) {
+    if (data2[i][0] === id) {
+      $("#invisibleID").val(data2[i][0]);
+      $("#inputName").val(data2[i][1]);
+      $("#inputAddress").val(data2[i][2]);
+      $("#inputNRIC").val(data2[i][3]);
+      $("#inputContactNo").val(data2[i][4]);
+      $("#inputEmail").val(data2[i][5]);
+      $("#inputCheckReferral").val(data2[i][6]);
+      $("#inputRegistrationDate").val(data2[i][7]);
+      $("#inputMedCond").val(data2[i][8]);
+      $("#inputDrugAlle").val(data2[i][9]);
+    }
+  }
 }
