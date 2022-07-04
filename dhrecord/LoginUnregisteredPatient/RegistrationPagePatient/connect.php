8c3fe07d42d9d79108d46530ab71bf0d60d0ced2
dhrecord <dentalrecord00@gmail.com>
2022-07-04 14:16:19 +0800

    .

--- dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/connect.php
+++ dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/connect.php
@@ -1,9 +1,9 @@
 <?php
 
-	$servername = "localhost";
-	$username = "u922342007_admin";
-	$password = "Aylm@012";
-	$database = "u922342007_Test";
+	//$servername = "localhost";
+	//$username = "u922342007_admin";
+	//$password = "Aylm@012";
+	//$database = "u922342007_Test";
 
 	$fullName = $_POST['fullName'];
 	$nricNumber = $_POST['nricNumber'];
@@ -14,16 +14,12 @@
 	$drug-allergies = $_POST['drug-allergies'];
 
 	//Database Connection
-	$conn = new mysqli($servername,$username,$password,$database);
+	$conn = new mysqli("localhost","u922342007_admin","Aylm@012","u922342007_Test");
 	
 	if(!$conn)
 	{
 		echo 'Not connected to server!';
 	}
-
-	    if(!mysqli_select_db($conn,$database)){
-        echo 'Database not selected!';
-    }
 
 	//else
 	//{
@@ -35,16 +31,16 @@
 		//$conn->close();
 	//}
 
-	if ($stmt = mysqli_prepare($conn, "insert into registration(fullName, nricNumber, contactNumber, email, address, med-conditions, drug-allergies) values(?, ?, ?, ?, ?, ?, ?)")) 
-	{
-		mysqli_stmt_bind_param($stmt, "sssssss", $fullName, $nricNumber, $contactNumber, $email, $address, $med-conditions, $drug-allergies);
-		mysqli_stmt_execute($stmt);
-		echo "Data inserted";
-    }
+	//if ($stmt = mysqli_prepare($conn, "insert into registration(fullName, nricNumber, contactNumber, email, address, med-conditions, drug-allergies) values(?, ?, ?, ?, ?, ?, ?)")) 
+	//{
+	//	mysqli_stmt_bind_param($stmt, "sssssss", $fullName, $nricNumber, $contactNumber, $email, $address, $med-conditions, $drug-allergies);
+	//	mysqli_stmt_execute($stmt);
+	//	echo "Data inserted";
+    //}
 
-   else 
-   {
-		echo "Error";
-   }
+   //else 
+   //{
+   //	echo "Error";
+   //}
 
 ?>
