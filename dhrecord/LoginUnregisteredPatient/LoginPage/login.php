a3e425c4ddaf4b5c646584c934eb912a67f2262d
xgladysx <gladysang55@gmail.com>
2022-07-08 00:36:23 +0800

    added session id and then referralTracking.php but idk how to merge into the referralTracking.html i hope nothing breaks bless us

--- dhrecord/LoginUnregisteredPatient/LoginPage/login.php
+++ dhrecord/LoginUnregisteredPatient/LoginPage/login.php
@@ -1,5 +1,5 @@
 <?php
-
+	session_start();
 	$userName = $_POST['userName'];
 	$passWord = $_POST['passWord'];
 
@@ -19,14 +19,19 @@
 	
 	else
 	{
-		$stmt = $conn->prepare("select * from registeredPatient where email = ?");
+		$stmt = $conn->prepare("SELECT ID, password FROM users where username = ?");
 		$stmt->bind_param("s", $userName);
 		$stmt->execute();
 		$stmt_result = $stmt->get_result();
 		if($stmt_result->num_rows > 0){
+			$stmt->bind_result($id, $password);
+			$stmt->fetch();
 			$data = $stmt_result->fetch_assoc();
 			if($data['password'] === $passWord) {
 				echo "Login Successful";
+				session_regenerate_id();
+				$_SESSION['loggedin'] = TRUE;
+				$_SESSION['id'] = $id;
 			}else{
 				echo "invalid username or password";
 			}
