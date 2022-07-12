dc232b6892442825dc9183d9f6aa0c85f5b62d4c
jovencsw <jovencsw@gmail.com>
2022-07-13 00:18:12 +0800

    testing

--- dhrecord/businessowner/html/addPrescription.php
+++ dhrecord/businessowner/html/addPrescription.php
@@ -9,6 +9,10 @@
 	$password = "Aylm@012";
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $database);
+	$prescriptionName = $_POST['prescriptionName'];
+	$prescriptionDesc = $_POST['prescriptionDesc'];
+	$prescriptionQty = $_POST['Quantity'];
+	$Remarks = $_POST['Remarks'];
 
 	if (!$conn) 
 	{
@@ -16,44 +20,24 @@
 	}
 	
 	//inserting data
-	//if ($stmt = mysqli_prepare($conn, "INSERT INTO inventoryManagement(prescriptionName,prescriptionDesc,prescriptionQty, Remarks) VALUES (?, ?, ?, ?)")) 
-	//if ($stmt = mysqli_prepare($conn, "INSERT INTO `inventoryManagement` (`prescriptionName`, `prescriptionDesc`, `prescriptionQty`, `Remarks`) VALUES ('', '', '', '')"))
-	//{
-	//	mysqli_stmt_bind_param($stmt, "ssssssss", $prescriptionName, $prescriptionDesc, $prescriptionQty, $Remarks);
-	//	mysqli_stmt_execute($stmt);
-	//	header("Location: http://dhrecord.com/dhrecord/businessowner/html/inventoryManagement.html");
-	//	mysqli_close($conn);
-    //}
-   //else 
-   //{
-   //		echo "Error";
-   //}
+	//if ($stmt = mysqli_prepare($conn, "insert into inventorymanagement(prescriptionname,prescriptiondesc,prescriptionqty, remarks) values (?, ?, ?, ?)")) 
+	if ($stmt = mysqli_prepare($conn, "insert into inventorymanagement(ID,prescriptionname, prescriptiondesc, prescriptionqty, remarks) values (default,?, ?, ?, ?)"))
+	{
+		
+		mysqli_stmt_bind_param($stmt, "ssssssss", $prescriptionname, $prescriptiondesc, $prescriptionqty, $remarks);
+		mysqli_stmt_execute($stmt);
+
+		header("location: http://dhrecord.com/dhrecord/businessowner/html/inventorymanagement.html");
+		mysqli_close($conn);
+    }
+
+   if ($stmt) { echo "success"; mysqli_close($conn); } else { echo "error"; mysqli_close($conn); }
   
 	   //if(!empty($_POST['prescriptionName']) && !empty($_POST['prescriptionDesc']) && !empty($_POST['Quantity']) && !empty($_POST['Remarks'])) {
 
-	   $prescriptionName = $_POST['prescriptionName'];
-		$prescriptionDesc = $_POST['prescriptionDesc'];
-		$prescriptionQty = $_POST['Quantity'];
-		$Remarks = $_POST['Remarks'];	
+	    	
 
-		$query = "INSERT INTO inventoryManagement(prescriptionName, prescriptionDesc, prescriptionQty, Remarks) VALUES ('$prescriptionName', '$prescriptionDesc', '$prescriptionQty', '$Remarks')"
-	   
-		$run = mysqli_query($conn,$query);
-
-		//if ($run){
-		//	echo "Form submitted successfully"
-		//}
-
-		//else {
-		//	echo "Form not submitted"
-		//}
-	  // }
-
-	   //else {
-		//   echo "all fields required"
-	   //}
-   
-   //$sql= "INSERT INTO `inventoryManagement` (`prescriptionName`, `prescriptionDesc`, `prescriptionQty`, `Remarks`) VALUES ('$prescriptionName', '$prescriptionDesc', '$prescriptionQty', '$Remarks')";
-	//header("Location: http://dhrecord.com/dhrecord/businessowner/html/AddNewPrescription.html");
+		   
+ 
 
 ?>
