3dc7ac2fb2bfc4a27ec5148ab528a7963d074d73
dhrecord <dentalrecord00@gmail.com>
2022-07-14 21:49:51 +0800

    pls pull

--- dhrecord/temporary_folder/connect.php
+++ dhrecord/temporary_folder/connect.php
@@ -1,44 +1,5 @@
 <?php
-
-	$fullName = $_POST['fullName'];
-	$nricNumber = $_POST['nricNumber'];
-	$contactNumber = $_POST['contactNumber'];
-	$email = $_POST['email'];
-	$registrationNumber = $_POST['registrationNumber'];
-	$licenseNumber = $_POST['licenseNumber'];
-	$locationOfClinic = $_POST['locationOfClinic'];
-	$clinicSpecialization = $_POST['clinicSpecialization'];
-
-	//Database Connection
-	$servername = "localhost";
-	$database = "u922342007_Test";
-	$username = "u922342007_admin";
-	$password = "Aylm@012";
-	// Create connection
-	$conn = mysqli_connect($servername, $username, $password, $database);
-
-	if (!$conn) 
-	{
-		die("Connection failed: " . mysqli_connect_error());
-	}
-	
-	//inserting data
-	if ($stmt = mysqli_prepare($conn, "insert into businessOwner(fullName, nricNumber, contactNumber, email, registrationNumber, licenseNumber, locationOfClinic, clinicSpecialization) values(?, ?, ?, ?, ?, ?, ?, ?)")) 
-	{
-		mysqli_stmt_bind_param($stmt, "ssssiiss", $fullName, $nricNumber, $contactNumber, $email, $registrationNumber, $licenseNumber, $locationOfClinic, $clinicSpecialization);
-		mysqli_stmt_execute($stmt);
-		header("Location: http://dhrecord.com/dhrecord/businessowner/LoginPage/");
-		mysqli_close($conn);
-    }
-   else 
-   {
-   		echo "Error";
-   }
-
-?>
-
-<?php
-
+	//cringe
 	//variables to go into registeredPatient
 	$fullName = $_POST['fullName'];
 	$nricNumber = $_POST['nricNumber'];
