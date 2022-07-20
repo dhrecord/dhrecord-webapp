5059efe0b0f6a55a69e636feb0d54daf01f39e4d
xgladysx <92320525+xgladysx@users.noreply.github.com>
2022-07-20 23:23:59 +0800

    FINAL

--- dhrecord/registeredpatient/html/referralTracking.php
+++ dhrecord/registeredpatient/html/referralTracking.php
@@ -26,7 +26,7 @@
         <div class="container-fluid container">
             <a class="navbar-brand" href="./index.html"><b>DHRecord</b></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarText">
@@ -35,19 +35,19 @@
                         <a class="nav-link" aria-current="page" href="./index.html">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="../apptScheduling/index.html">
+                        <a class="nav-link" href="./apptSchedulingAndReminders.html">
                             Appointment Scheduling &
                             Reminder
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="./referralTracking.html">Referral Tracking</a>
+                        <a class="nav-link active" href="./referralTracking.php">Referral Tracking</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="./surveyAndFeedback.html">Survey & Feedback</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="./treatmentPlanning.html">Treatment Planning</a>
+                        <a class="nav-link" href="./treatmentPlanning.html">Treatment Planning</a>
                     </li>
                 </ul>
                 <div class="d-flex flex-column align-items-end">
@@ -55,7 +55,7 @@
                         Welcome, Username
                     </p>
                     <button type="button" class="btn btn-light ml-3 btn-sm mb-2" style="width: 90px;"
-                        onclick="document.location.href='../../LoginUnregisteredPatient/LoginPage/index.html'">
+                            onclick="document.location.href='../../LoginUnregisteredPatient/LoginPage/index.php'">
                         Logout
                     </button>
                 </div>
@@ -65,65 +65,54 @@
 
     <!-- content -->
     <div class="container my-5">
-        <h4 class="mb-5">Treatment Planning</h4>
-        <div class="mb-4 d-flex align-items-center">
-            <div class="d-flex align-items-center">
-                <p class="m-0"><b>Search:</b>&nbsp;&nbsp;&nbsp;</p>
-                <div class="input-group">
-                    <input type="text" id="searchNameInput" class="form-control" placeholder="Enter Value ..."
-                        aria-label="Name" aria-describedby="basic-addon2" style="max-width: 300px;" />
-                    <button class="input-group-text" id="basic-addon2" onclick="searchName();">
-                        <i class="fa-solid fa-magnifying-glass"></i>
-                    </button>
-                </div>
-            </div>
-            <select class="form-select" id="userManagement_ddlFilterBy" aria-label="Filter By..."
-                style="margin-left: 70px; max-width: 250px;">
-                <option selected disabled hidden>Filter By...</option>
-                <option value="1">Current treatment plan</option>
-                <option value="2">Next treatment plan</option>
-            </select>
-        </div>
-        <table class="table table-striped">
-			<tr>
-				<th>startDate</th>
-				<th>endDate</th>
-				<th>details</th>
-			</tr>    		
-			<tr>
-			 <?php                           
-				session_start();
-				//Database Connection
-				$servername = "localhost";
-				$database = "u922342007_Test";
-				$username = "u922342007_admin";
-				$password = "Aylm@012";
-	
-				// Create connection
-				$conn = mysqli_connect($servername, $username, $password, $database);
+        <h4 class="mb-5">Referral Tracking</h4>
+			<table class="table table-striped">
+        		<tr>
+        			<th>referredBy</th>
+        			<th>referralDate</th>
+        		</tr>    		
+                
+        		<tr>
+                 <?php                           
+        	session_start();
+                    //Database Connection
+                    $servername = "localhost";
+                    $database = "u922342007_Test";
+                    $username = "u922342007_admin";
+                    $password = "Aylm@012";
+        
+                    // Create connection
+                    $conn = mysqli_connect($servername, $username, $password, $database);
+                
+        
+                    //$getid = $_SESSION['id'];
+        	        $res = ("SELECT referralTracking.referredBy, referralTracking.referralDate FROM referralTracking, registeredPatient, users
+        			WHERE users.ID = '{$_SESSION['id']}' AND users.ID = registeredPatient.users_ID AND registeredPatient.ID = referralTracking.patient_ID");
+
+			        $result = mysqli_query($conn, $res);
+					
+
+                    while($sql = mysqli_fetch_assoc($result)){
+                              echo "<tr><td>".$sql["referredBy"]."</td><td>".$sql["referralDate"]."</td></tr>";
+                            }
+			        ?>         
+
+        	</table>
+        
+    
+		
 			
-	
-				//$getid = $_SESSION['id'];
-				$res = ("SELECT `treatmentPlan.startDate`, `treatmentPlan.endDate`, `treatmentPlan.details` FROM `treatmentPlan`, `registeredPatient`, `users`
-				WHERE `users.ID` = '{$_SESSION['id']}' AND `users.ID` = `registeredPatient.users_ID` AND `registeredPatient.ID` = `treatmentPlan.regPt_ID`");
-
-				$result = mysqli_query($conn, $res);
-				
-
-				while($sql = mysqli_fetch_assoc($result)){
-					echo "<tr><td>".$sql["startDate"]."</td><td>".$sql["endDate"]."</td>"."<td>".$sql["details"]."</td></tr>";
-				}
-				?>
-        </table>
     </div>
 
 
     <!-- bootstrap js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"></script>
 
     <script src="../js/index.js"></script>
+
+
 </body>
 
 </html>
