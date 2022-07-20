2c041d447f1ec0472dd505a25eef329e2cd8a29b
xgladysx <92320525+xgladysx@users.noreply.github.com>
2022-07-20 20:53:51 +0800

    Update referralTracking.php

--- dhrecord/registeredpatient/html/referralTracking.php
+++ dhrecord/registeredpatient/html/referralTracking.php
@@ -41,7 +41,7 @@
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link active" href="./referralTrackingTest.php">Referral Tracking</a>
+                        <a class="nav-link active" href="./referralTracking.php">Referral Tracking</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="./surveyAndFeedback.html">Survey & Feedback</a>
