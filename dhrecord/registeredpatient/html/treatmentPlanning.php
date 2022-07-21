e4f836babeadbe6b032e73b67206a957fa1f42c9
xgladysx <92320525+xgladysx@users.noreply.github.com>
2022-07-21 16:21:34 +0800

    Update treatmentPlanning.php

--- dhrecord/registeredpatient/html/treatmentPlanning.php
+++ dhrecord/registeredpatient/html/treatmentPlanning.php
@@ -104,7 +104,7 @@
 			
 	
 				$res = ("SELECT `treatmentPlan.startDate`, `treatmentPlan.endDate`, `treatmentPlan.details` FROM `treatmentPlan`, `registeredPatient`, `users`
-					WHERE `users.ID` = `{$_SESSION['id']}` AND `users.ID` = `registeredPatient.users_ID` AND `registeredPatient.ID` = `treatmentPlan.regPt_ID`");
+					WHERE `users.ID` === `{$_SESSION['id']}` AND `users.ID` === `registeredPatient.users_ID` AND `registeredPatient.ID` === `treatmentPlan.regPt_ID`");
 								
 				$result = mysqli_query($conn, $res);
 				
