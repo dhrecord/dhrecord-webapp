d3188b65274323d875e19bb3858824c6e65d0656
xgladysx <92320525+xgladysx@users.noreply.github.com>
2022-07-21 21:55:34 +0800

    Update treatmentPlanning.php

--- dhrecord/registeredpatient/html/treatmentPlanning.php
+++ dhrecord/registeredpatient/html/treatmentPlanning.php
@@ -111,7 +111,7 @@
 
 
 			while($sql = mysqli_fetch_assoc($result)){
-				echo "<td>".$sql["startDate"]."</td><td>".$sql["endDate"]."</td><td>".$sql["details"]."</td>";
+				echo $sql["startDate"];
 			}
 			?>
         </table>
