343fa14545be120021360df4c785cdcd67b78ce9
xgladysx <92320525+xgladysx@users.noreply.github.com>
2022-07-22 15:35:32 +0800

    Update treatmentPlanning.php

--- dhrecord/registeredpatient/html/treatmentPlanning.php
+++ dhrecord/registeredpatient/html/treatmentPlanning.php
@@ -108,11 +108,11 @@
 				
 			echo "'{$_SESSION['id']}'";
 			
-			$res = ("SELECT * FROM treatmentPlan WHERE regPt_ID = '{$_SESSION['id']}'");
+			/*$res = ("SELECT * FROM treatmentPlan WHERE regPt_ID = '{$_SESSION['id']}'");*/
 				
-			/*$res = ("SELECT treatmentPlan.startDate, treatmentPlan.endDate, treatmentPlan.details FROM treatmentPlan, registeredPatient, users
+			$res = ("SELECT treatmentPlan.startDate, treatmentPlan.endDate, treatmentPlan.details FROM treatmentPlan, registeredPatient, users
 				WHERE users.ID = '{$_SESSION['id']}' AND users.ID = registeredPatient.users_ID AND registeredPatient.ID = treatmentPlan.regPt_ID");
-			*/
+			
 			$result = mysqli_query($conn, $res);
 			echo $result;
 			
