ed40499390603ee4c809ab70e891e36c9ac48ed8
xgladysx <92320525+xgladysx@users.noreply.github.com>
2022-07-24 17:21:01 +0800

    Update logout.php

--- dhrecord/LoginUnregisteredPatient/logout.php
+++ dhrecord/LoginUnregisteredPatient/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-/*unset($_SESSION["id"]);
-unset($_SESSION["username"]);*/
+unset($_SESSION["id"]);
+unset($_SESSION["username"]);
 session_destroy();
 // Redirect to the login page:
 header('Location: http://www.dhrecord.com/dhrecord/LoginUnregisteredPatient/LoginPage/index.html');
