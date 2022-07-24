9e4bc062514e9495d1ba9d0e801f96e3c3789225
dhrecord <dentalrecord00@gmail.com>
2022-07-24 22:55:20 +0800

    .

--- dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/verify.php
+++ dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/verify.php
@@ -1,5 +1,46 @@
 <?php 
 
-echo "owo";
+	//Database Connection
+	$servername = "localhost";
+	$database = "u922342007_Test";
+	$username = "u922342007_admin";
+	$password = "Aylm@012";
+
+	// Create connection
+	$conn = mysqli_connect($servername, $username, $password, $database);
+
+	if (!$conn) 
+	{
+		die("Connection failed: " . mysqli_connect_error());
+	}
+
+if(isset($_GET['vkey'])){
+
+	$vkey = $_GET['vkey'];
+	$fetchVerified = mysqli_prepare($conn, "SELECT verified,vkey FROM tempRegisteredPatient WHERE verified = 0 AND vkey = '$vkey' LIMIT 1");
+	$fetchVerified->execute();
+	$resultSet = $fetchVerified->get_result();
+
+	if($resultSet->num_rows == 1){
+		 //change verified from 0 to 1
+		 $update = mysqli_prepare($conn, "UPDATE tempRegisteredPatient SET verified = 1 WHERE vkey = '$vkey' LIMIT 1");
+		 $update->execute();
+		 
+		 if ($update)
+		 {
+			 echo "Your account has been verified! Please await approval from our staff before login!";
+		 }
+		 else
+		 {
+			 echo "something went wrong!";
+		 }
+
+	}
+	else{
+		echo "This account is invalid or already verified";
+	}
+}else{
+	die("Something went wrong D:");
+}
 
 ?>
