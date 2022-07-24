a28358917e06425dff34acc4d81c007265ef0df5
dhrecord <dentalrecord00@gmail.com>
2022-07-24 13:11:40 +0800

    .

--- dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/connect.php
+++ dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/connect.php
@@ -14,11 +14,15 @@
 	$userName = $_POST['userName'];
 	$passWord = $_POST['passWord'];
 
+	//timestamp
+	$vkey = md5(time().$userName);
+
 	//Database Connection
 	$servername = "localhost";
 	$database = "u922342007_Test";
 	$username = "u922342007_admin";
 	$password = "Aylm@012";
+
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $database);
 
@@ -27,11 +31,15 @@
 		die("Connection failed: " . mysqli_connect_error());
 	}
 
-	//$subject = "Email Verification";
-	//$message = "Hi! Please click the link below to verify your email! <a href='http://localhost/'>"
-	//$headers = "";
+	$subject = "Email Verification";
+	$message = "Hi! Please click the link below to verify your email!<br> <a href='http://dhrecord.com/dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/verify.php'>Register Account</a>"
+	$headers = "From: dentalrecord00@gmail.com \r\n";
+	$headers .= "MIME-version: 1.0" . "\r\n";
+	$headers .= "content-type:text/html;charset=UTF-8" . "\r\n";
+
+	mail($email,$subject,$message,$headers);
 	
-	$stmt = mysqli_prepare($conn, "insert into tempRegisteredPatient(fullName, nricNumber, contactNumber, email, address, medConditions, drugAllergies, role, username, password) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+	$stmt = mysqli_prepare($conn, "insert into tempRegisteredPatient(fullName, nricNumber, contactNumber, email, address, medConditions, drugAllergies, role, username, password) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 	mysqli_stmt_bind_param($stmt, "ssssssssss", $fullName, $nricNumber, $contactNumber, $email, $address, $medConditions, $drugAllergies, $role, $userName, $passWord);
 	mysqli_stmt_execute($stmt);
 
