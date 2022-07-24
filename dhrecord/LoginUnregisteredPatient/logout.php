145a192c4bded9ff49bc7d9442c9ec5c65597a04
xgladysx <92320525+xgladysx@users.noreply.github.com>
2022-07-24 18:08:05 +0800

    Update logout.php

--- dhrecord/LoginUnregisteredPatient/logout.php
+++ dhrecord/LoginUnregisteredPatient/logout.php
@@ -2,8 +2,11 @@
 session_start();
 
 //unset($_SESSION["id"], $_SESSION["username"])
-session_unset();
+//session_unset();
+
+$_SESSION = array();
 session_destroy();
+
 
 /*unset($_SESSION["id"]);
 unset($_SESSION["username"]);
