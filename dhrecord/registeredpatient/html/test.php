ebcf2f69b49d5b1ef309100d5263d7dbf06df4d6
vanchio <108721199+vanchio@users.noreply.github.com>
2022-07-25 15:01:32 +0800

    haiz

--- dhrecord/registeredpatient/html/test.php
+++ dhrecord/registeredpatient/html/test.php
@@ -1,6 +1,6 @@
 <?php
 
-  $uName=$_SESSION['username'];
+  //$uName=$_SESSION['username'];
   $estimatedWait=$_POST['estimatedWait'];
   $serviceQuality=$_POST['serviceQuality'];
   $recommended=$_POST['recommended'];
@@ -13,10 +13,10 @@
 
   $conn = mysqli_connect($servername, $username, $password, $database);
 
-  $stmt = mysqli_prepare($conn, "insert into `surveyForm`(`username`,`timeTaken`, `rating`, `recommendation`,`remarks`) values (?, ?, ?, ?, ?)");
-	mysqli_stmt_bind_param($stmt, "ssis", $uName, $estimatedWait, $serviceQuality, $recommended, $medconds);
- //$stmt = mysqli_prepare($conn, "insert into `surveyForm`(`timeTaken`, `rating`, `recommendation`,`remarks`) values (?, ?, ?, ?)");
-	//mysqli_stmt_bind_param($stmt, "ssis", $estimatedWait, $serviceQuality, $recommended, $medconds);
+  //$stmt = mysqli_prepare($conn, "insert into `surveyForm`(`username`,`timeTaken`, `rating`, `recommendation`,`remarks`) values (?, ?, ?, ?, ?)");
+	//mysqli_stmt_bind_param($stmt, "ssis", $uName, $estimatedWait, $serviceQuality, $recommended, $medconds);
+ $stmt = mysqli_prepare($conn, "insert into `surveyForm`(`timeTaken`, `rating`, `recommendation`,`remarks`) values (?, ?, ?, ?)");
+	mysqli_stmt_bind_param($stmt, "ssis", $estimatedWait, $serviceQuality, $recommended, $medconds);
 	mysqli_stmt_execute($stmt);
 
   header("Location: http://dhrecord.com/dhrecord/registeredpatient/html/surveyAndFeedback.php");
