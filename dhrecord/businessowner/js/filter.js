3b18c6e33c0d42a23599be70a0ad1af52f11d428
xgladysx <92320525+xgladysx@users.noreply.github.com>
2022-07-26 20:59:42 +0800

    Create filter.js

---
+++ dhrecord/businessowner/js/filter.js
@@ -0,0 +1,28 @@
+//get dropdown list and searchbar
+const dropdown = document.getElementById("referralTracking_ddlfilter");
+const searchbar = document.getElementById("searchbar_filter")
+//get td rows
+const table_body = document.querySelector("#data");
+const rows = table_body.querySelectorAll("tr");
+
+//add eventlistener for filter dropdown list. this basically checks for any action done. for example, onclick, onchange...
+//dropdownlist event
+searchbar.addEventListener("input", (evt) =>{
+    try {
+        const q = evt.currentTarget.value.toLowerCase(); //standardize lowercase for matching
+        const value = dropdown.value;
+        if(value) { //if dropdown list is not empty string, meaning dropdown is selected
+            rows.forEach(row => {
+                const matches = row[value].textContent.toLowerCase().startsWith(q);
+                row.style.display = matches ? "table-row" : "none";
+            })
+        }
+        else {
+            alert("please select a category from the dropdown list")
+        }
+    }
+    catch (e){
+        console.log(e);
+    }
+     
+})
