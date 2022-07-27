e5f3db846ecfe4e3f9b03a8bb9b7386670fc34c6
dhrecord <dentalrecord00@gmail.com>
2022-07-27 15:04:13 +0800

    .

--- dhrecord/superadmin/html/registrationmanagement_businessowner.php
+++ dhrecord/superadmin/html/registrationmanagement_businessowner.php
@@ -23,51 +23,49 @@
 
 <body onload="findData3();">
     <!-- navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid container">
-            <a class="navbar-brand" href="./home.php"><b>DHRecord</b></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./home.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./clinicspecialization.html">Clinic Specialization</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./auditlog.html">Audit Log</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./usermanagement.html">User Management</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Registration Management
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="./registrationmanagement_businessowner.html">Business
-                                    Owner</a></li>
-                            <li><a class="dropdown-item" href="./registrationmanagement_patientaccount.html">Patient
-                                    Account</a></li>
-                        </ul>
-                    </li>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid container">
+      <a class="navbar-brand" href="./home.php"><b>DHRecord</b></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+        aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="./home.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./clinicspecialization.php">Clinic Specialization</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./auditlog.php">Audit Log</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./usermanagement.php">User Management</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Registration Management
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="./registrationmanagement_businessowner.php">Business Owner</a></li>
+              <li><a class="dropdown-item" href="./registrationmanagement_patientaccount.php">Patient Account</a></li>
+            </ul>
+          </li>
 
-                </ul>
-                <div class="d-flex flex-column align-items-end">
-                    <p class="navbar-text text-white m-0">
-                        Welcome, Username
-                    </p>
-                    <button type="button" class="btn btn-light ml-3 btn-sm mb-2" style="width: 90px;"
-                        onclick="document.location.href='./login.html'">Logout</button>
-                </div>
-            </div>
+        </ul>
+        <div class="d-flex flex-column align-items-end">
+          <p class="navbar-text text-white m-0">
+            Welcome, <?php echo $_SESSION['username']; ?>
+          </p>
+          <button type="button" class="btn btn-light ml-3 btn-sm mb-2" style="width: 90px;"
+            onclick="document.location.href='../../LoginUnregisteredPatient/LoginPage/logout.php'">Logout</button>
         </div>
-    </nav>
+      </div>
+    </div>
+  </nav>
 
     <!-- content -->
     <div class="container my-5">
