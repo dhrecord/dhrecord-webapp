0bf92cca7ededf930120d52a59c1415de9dfd112
xgladysx <92320525+xgladysx@users.noreply.github.com>
2022-07-28 17:35:24 +0800

    session ID

--- dhrecord/businessowner/html/addPrescription.php
+++ dhrecord/businessowner/html/addPrescription.php
@@ -1,4 +1,10 @@
 <?php
+	session_start();
+  	if(!isset($_SESSION['loggedin']))
+  	{
+    		header('Location: ../../LoginUnregisteredPatient/LoginPage/index.html');
+    		exit;
+  	}
 
 	//$ID = 'default';
 	$prescriptionName = $_POST['prescriptionName'];
