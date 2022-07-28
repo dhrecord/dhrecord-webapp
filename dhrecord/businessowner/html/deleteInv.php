8a9882e32071f0eaba30759808c2d8e5f5543d3a
xgladysx <92320525+xgladysx@users.noreply.github.com>
2022-07-28 18:06:27 +0800

    session

--- dhrecord/businessowner/html/deleteInv.php
+++ dhrecord/businessowner/html/deleteInv.php
@@ -1,5 +1,10 @@
 <?php
-
+	session_start();
+	if(!isset($_SESSION['loggedin']))
+	{
+		header('Location: ../../LoginUnregisteredPatient/LoginPage/index.html');
+	    	exit;
+	}
 	require_once("connection.php");	
 
 if(isset($_GET['Delete']))
