5504296569eb387cc5d59fd364e13542986e0941
Aurellya <aurellyagho25@gmail.com>
2022-07-29 00:16:14 +0800

    [add missing =]

--- dhrecord/registeredpatient/html/treatmentHistory.php
+++ dhrecord/registeredpatient/html/treatmentHistory.php
@@ -22,7 +22,7 @@ if(!isset($_SESSION['loggedin']))
     <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-	<link rel="stylesheet" href"https://cdn.jsdeliver.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"/>
+	<link rel="stylesheet" href="https://cdn.jsdeliver.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"/>
 
     <!-- fontawesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
