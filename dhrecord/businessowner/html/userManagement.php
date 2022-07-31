73842892a6d6bb8f3d8f293a25ab623b8078c852
dhrecord <dentalrecord00@gmail.com>
2022-07-31 23:10:21 +0800

    .

--- dhrecord/businessowner/html/userManagement.php
+++ dhrecord/businessowner/html/userManagement.php
@@ -120,7 +120,6 @@
             <select class="form-select" id="userManagement_ddlFilterBy" aria-label="Filter By..."
                 style="margin-left: 70px; max-width: 250px;">
                 <option selected disabled hidden>Filter By...</option>
-                <option value="1">No</option>
                 <option value="2">Name</option>
                 <option value="3">Address</option>
                 <option value="4">NRIC</option>
@@ -135,109 +134,84 @@
         <table class="table table-striped">
             <thead>
                 <tr>
-                    <th scope="col">No</th>
                     <th scope="col">Name</th>
+                    <th scope="col">Role</th>
                     <th scope="col">Contact No</th>
                     <th scope="col">Email</th>
                     <th scope="col">Edit</th>
                     <th scope="col">Delete</th>
                 </tr>
             </thead>
-            <tbody id="data">
-                <!-- <tr>
-                    <th scope="row">1</th>
-                    <td>John Doe</td>
-                    <td>5 Magazine Road #02-01, 059571, Singapore</td>
-                    <td>S5219994H</td>
-                    <td>+65 8950 4262</td>
-                    <td>JohnDoe@gmail.com</td>
-                    <td class="text-center"><button class="border-0 edit-btn" data-bs-toggle="modal"
-                            data-bs-target="#popupModal"><i class="fa-solid fa-pen-to-square"></i></button>
-                    </td>
-                    <td class="text-center"><button class="border-0"><i class="fa-solid fa-trash-can"></i></button></td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jane Doe</td>
-                    <td>926 Yishun Central 1 01-177, 760926, Singapore</td>
-                    <td>S6891261Z</td>
-                    <td>+65 8952 7201</td>
-                    <td>JaneDoe@gmail.com</td>
-                    <td class="text-center"><button class="border-0 edit-btn" data-bs-toggle="modal"
-                            data-bs-target="#popupModal"><i class="fa-solid fa-pen-to-square"></i></button>
-                    </td>
-                    <td class="text-center"><button class="border-0"><i class="fa-solid fa-trash-can"></i></button></td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Nate</td>
-                    <td>1 Coleman Street 02-30/31 The Adelphi, 179803, Singapore</td>
-                    <td>S9168686D</td>
-                    <td>+65 9786 5789</td>
-                    <td>Nate22@gmail.com</td>
-                    <td class="text-center"><button class="border-0 edit-btn" data-bs-toggle="modal"
-                            data-bs-target="#popupModal"><i class="fa-solid fa-pen-to-square"></i></button>
-                    </td>
-                    <td class="text-center"><button class="border-0"><i class="fa-solid fa-trash-can"></i></button></td>
-                </tr>
-                <tr>
-                    <th scope="row">4</th>
-                    <td>Jack</td>
-                    <td>112 East Coast Road #B1-14 112 Katong, 428802, Singapore</td>
-                    <td>S5397679D</td>
-                    <td>+65 8951 7299</td>
-                    <td>Jack11@gmail.com</td>
-                    <td class="text-center"><button class="border-0 edit-btn" data-bs-toggle="modal"
-                            data-bs-target="#popupModal"><i class="fa-solid fa-pen-to-square"></i></button>
-                    </td>
-                    <td class="text-center"><button class="border-0"><i class="fa-solid fa-trash-can"></i></button></td>
-                </tr> -->
+            <tbody>
+            <?php
+
+                $query = "SELECT * FROM clinicAdmin WHERE clinicID = $clinicID";
+
+                if ($result = $conn->query($query)) 
+                {
+                    while ($row = $result->fetch_assoc()) 
+                    {
+                        $clinicadminID = $row["clinicadminID"];
+                        $query1 = "SELECT * FROM users WHERE ID = $clinicadminID";
+                        $result1 = $conn->query($query1);
+                        $row1 = $result1->fetch_assoc();
+                ?>
+                    <tr>
+                        <td><?php echo $row["fullName"]; ?></td>
+                        <td><?php echo $row1["ID"]; ?></td> 
+                        <td><?php echo $row["contactNumber"]; ?></td> 
+                        <td><?php echo $row["email"]; ?></td>
+                        <td><button class="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#popupModal<?php echo $row["clinicadminID"]; ?>">Edit</button></td>
+                        <td><button type="button" class="btn btn-sm btn-success" onclick="document.location.href='deleteUser.php?clinicadminID=<?php echo $row["clinicadminID"]; ?>'">Delete</button></td>
+                    </tr>
+
+
+                <?php
+                    }
+                }
+                ?>
             </tbody>
         </table>
 
         <!-- modal -->
-        <div class="modal fade" id="popupModal" tabindex="-1" aria-labelledby="popupModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
+        <div class="modal fade" id="popupModal<?php echo $row["clinicadminID"]; ?>" tabindex="-1" aria-labelledby="popupModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="popupModalLabel">Edit Information</h5>
+                        <h5 class="modal-title" id="popupModalLabel">View/Edit Full Information</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div class="modal-body">
+                    <div class="modal-body" action="./editUserClinicAdmin.php" method="post">
                         <form>
                             <p style="display: none;" id="invisibleID"></p>
                             <div class="mb-3">
-                                <label for="inputName" class="form-label">Full Name</label>
-                                <input type="text" class="form-control" id="inputName">
+                                <label for="clinicadminID" class="form-label">Clinic Admin ID</label>
+                                <input type="text" class="form-control" id="clinicadminID" <?php echo 'value="'.$row["clinicadminID"].'"'; ?>>
                             </div>
                             <div class="mb-3">
-                                <label for="inputAddress" class="form-label">Address</label>
-                                <input type="text" class="form-control" id="inputAddress">
+                                <label for="fullName" class="form-label">Full Name</label>
+                                <input type="text" class="form-control" id="fullName" <?php echo 'value="'.$row["fullName"].'"'; ?>>
                             </div>
                             <div class="mb-3">
-                                <label for="inputNRIC" class="form-label">NRIC</label>
-                                <input type="text" class="form-control" id="inputNRIC">
+                                <label for="contactNumber" class="form-label">Contact Number</label>
+                                <input type="text" class="form-control" id="contactNumber" <?php echo 'value="'.$row["contactNumber"].'"'; ?>>
                             </div>
                             <div class="mb-3">
-                                <label for="inputContactNo" class="form-label">Contact No.</label>
-                                <input type="text" class="form-control" id="inputContactNo">
+                                <label for="email" class="form-label">Email</label>
+                                <input type="text" class="form-control" id="email" <?php echo 'value="'.$row["email"].'"'; ?>>
+                            </div>
+                            <div class="mb-3">
+                                <label for="clinicID" class="form-label">Clinic ID</label>
+                                <input type="text" class="form-control" id="inputContactNo" <?php echo 'value="'.$row["clinicID"].'"'; ?> disabled>
                             </div>
                             <div class="mb-3">
                                 <label for="inputEmail" class="form-label">Email address</label>
-                                <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp">
-                                <div id="emailHelp" class="form-text">
-                                    We'll never share your email with anyone else.
-                                </div>
+                                <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" <?php echo 'placeholder="'.$email.'"'; ?> readonly>
                             </div>
-                            <div class="mb-3">
-                                <label for="inputCheckReferral" class="form-label">Check Referral</label>
-                                <input type="text" class="form-control" id="inputCheckReferral">
+                            <div class="mb-3 row">
+                                <div class="text-center"><button type="submit" class="btn btn-dark mt-4 px-5">Submit</button></div>
                             </div>
                         </form>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-                        <button type="button" onclick="saveDetails();" class="btn btn-primary">Save changes</button>
                     </div>
                 </div>
             </div>
