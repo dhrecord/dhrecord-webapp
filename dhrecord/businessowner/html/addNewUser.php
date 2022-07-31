1d202b46f4ce4a1d5a9a3184d69f3ebc2998a656
dhrecord <dentalrecord00@gmail.com>
2022-07-31 16:25:00 +0800

    .

--- dhrecord/businessowner/html/addNewUser.php
+++ dhrecord/businessowner/html/addNewUser.php
@@ -1,162 +1,68 @@
-<?php
-  session_start();
-  if(!isset($_SESSION['loggedin']))
-  {
-    header('Location: ../../LoginUnregisteredPatient/LoginPage/index.html');
-    exit;
-  }
+<?php 
 
-  $sessionID = $_SESSION['id'];
-  
-  $query = "SELECT * FROM businessOwner WHERE users_ID=$sessionID";
-  $clinicInfo = mysqli_query($conn,$query);
-  $row = $clinicInfo->fetch_assoc();
-  
-  $clinicID = $row['ID'];
+session_start();
+
+$role = $_POST['role'];
+$fullName = $_POST['fullName'];
+$contactNumber = $_POST['contactNumber'];
+$email = $_POST['email'];
+$userName = $_POST['userName'];
+$passWord = $_POST['passWord'];
+
+//Database Connection
+$servername = "localhost";
+$database = "u922342007_Test";
+$username = "u922342007_admin";
+$password = "Aylm@012";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) 
+{
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+$sessionID = $_SESSION['id'];
+$query = "SELECT * FROM businessOwner WHERE users_ID=$sessionID";
+$clinicInfo = mysqli_query($conn,$query);
+$row = $clinicInfo->fetch_assoc();
+
+$clinicID = $row['ID'];
+
+//inserting data
+$stmt = mysqli_prepare($conn, "insert into users(role, username, password) values (?, ?, ?)");
+mysqli_stmt_bind_param($stmt, "sss", $role, $userName, $passWord);
+mysqli_stmt_execute($stmt);
+
+$stmt = $conn->prepare("SELECT ID FROM users where username = ?");
+$stmt->bind_param("s", $userName);
+$stmt->execute();
+$stmt_result = $stmt->get_result();
+$row1 = $stmt_result->fetch_assoc();
+
+//echo $row1['ID'];
+
+//if($role === "dr")
+//{
+//
+//}
+
+if($role === "ca")
+{
+	$stmt = mysqli_prepare($conn, "insert into clinicAdmin(userID, fullName, contactNumber, email, clinicID) values(?, ?, ?, ?, ?)");
+	mysqli_stmt_bind_param($stmt, "isssi", $row1['ID'], $fullName, $contactNumber, $email, $clinicID);
+	mysqli_stmt_execute($stmt);
+	header('Location: addNewUser.php');
+}
+
+if($role === "fd")
+{
+	$stmt = mysqli_prepare($conn, "insert into frontDesk(userID, fullName, contactNumber, email, clinicID) values(?, ?, ?, ?, ?)");
+	mysqli_stmt_bind_param($stmt, "isssi", $row1['ID'], $fullName, $contactNumber, $email, $clinicID);
+	mysqli_stmt_execute($stmt);
+	header('Location: addNewUser.php');
+}
+
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>DHRecord</title>
-    <link rel="stylesheet" href="../css/style.css" />
-
-    <!-- bootstrap css -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-</head>
-
-<body>
-    <!-- navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid container">
-            <a class="navbar-brand" href="./index.html"><b>DHRecord</b></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./index.php">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./userManagement.php">User Management</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./referralTracking.php">Referral Tracking</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Appointment & Treatment
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="./apptSchedulingAndReminders.php">Appointment Scheduling
-                                    & Reminders</a></li>
-                            <li><a class="dropdown-item" href="./treatmentHistory.php">Treatment History</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./reportingAndStatistics.php">Reporting & Statistics</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./billingInvoicing.php">Payment</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" href="./inventoryManagement.php">Inventory Management</a>
-                    </li>
-                </ul>
-                <div class="d-flex flex-column align-items-end">
-                    <p class="navbar-text text-white m-0">
-                        Welcome, <?php echo $_SESSION['username'];?>
-                    </p>
-                    <button type="button" class="btn btn-light ml-3 btn-sm mb-2"
-                        onclick="document.location.href='../../LoginUnregisteredPatient/LoginPage/logout.php'">Logout</button>
-                </div>
-            </div>
-        </div>
-    </nav>
-
-    <!-- content -->
-    <div class="container my-5">
-        <h4 class="mb-4">Add New User</h4>
-        <form method="post" action="./addUser.php" method="POST">
-            <div class="container border border-dark p-4" style="border-top-width: 10px!important;">
-                <div class="mb-3 row">
-                    <label for="clinicID" class="col-sm-2 col-form-label">Clinic ID</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="clinicID" name="clinicID" <?php echo 'value = "'.$clinicID. '"'; ?> disabled> 
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="role" class="col-sm-2 col-form-label">Role</label>
-                    <div class="col-sm-10">
-                        <select name="role" id="role">
-                        <option value="dr">Doctor</option>
-                        <option value="ca">Clinic Admin</option>
-                        <option value="fd">Front Desk</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="fullName" class="col-sm-2 col-form-label">Full Name</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="fullName" name="fullName">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="contactNumber" class="col-sm-2 col-form-label">Contact Number</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="contactNumber" name="contactNumber">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="email" class="col-sm-2 col-form-label">Email</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="email" name="email">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="username" class="col-sm-2 col-form-label">Username</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="userName" name="userName">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <label for="passWord" class="col-sm-2 col-form-label">Password</label>
-                    <div class="col-sm-10">
-                        <input type="password" class="form-control" id="passWord" name="passWord">
-                    </div>
-                </div>
-                <div class="mb-3 row">
-                    <div class="text-center"><button type="submit" class="btn btn-dark mt-4 px-5">Submit</button></div>
-                </div>
-            </div>
-        </form>
-    </div>
-
-    <!-- bootstrap js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
-
-
-    <!-- content -->
-
-    <!-- bootstrap js -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossorigin="anonymous"></script>
-</body>
-
-</html>
