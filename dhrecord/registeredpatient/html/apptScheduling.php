d7ca375873f42681351b68cde4fa1d2925f74f8f
Aurellya <aurellyagho25@gmail.com>
2022-08-03 00:06:45 +0800

    [probably need an escape character?]

--- dhrecord/registeredpatient/html/apptScheduling.php
+++ dhrecord/registeredpatient/html/apptScheduling.php
@@ -391,11 +391,11 @@
       if (operatingHours !== '-'){
         let oh_item = operatingHours.split(', ');
         let oh_html = "<p>";
-        
+
         for (let i = 0; i < oh_item.length; i++) {
           let oh_item_day = "";
           oh_item_day += oh_item[i].substring(1, oh_item[i].length-1);
-          oh_item_day += "<br/>";
+          oh_item_day += "<br\/>";
 
           oh_html += oh_item_day;
         }
