239d7260dee9098be3d8d7cf23b47f4dc731667b
dhrecord <dentalrecord00@gmail.com>
2022-08-02 17:19:44 +0800

    .

---
+++ dhrecord/registeredpatient/html/passwordsDontMatch.php
@@ -0,0 +1,101 @@
+<?php
+  session_start();
+  if(!isset($_SESSION['loggedin']))
+  {
+    header('Location: ../../LoginUnregisteredPatient/LoginPage/index.html');
+    exit;
+  }
+
+  $sessionID = $_SESSION['id'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>DHRecord</title>
+    <link rel="stylesheet" href="../css/style.css" />
+
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid container">
+            <a class="navbar-brand" href="./index.html"><b>DHRecord</b></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="./index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./userManagement.php">User Management</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./referralTracking.php">Referral Tracking</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Appointment & Treatment
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="./apptSchedulingAndReminders.php">Appointment Scheduling
+                                    & Reminders</a></li>
+                            <li><a class="dropdown-item" href="./treatmentHistory.php">Treatment History</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./reportingAndStatistics.php">Reporting & Statistics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./billingInvoicing.php">Payment</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="./inventoryManagement.php">Inventory Management</a>
+                    </li>
+                </ul>
+                <div class="d-flex flex-column align-items-end">
+                    <p class="navbar-text text-white m-0">
+                        Welcome, <?php echo $_SESSION['username'];?>
+                    </p>
+                    <button type="button" class="btn btn-light ml-3 btn-sm mb-2"
+                        onclick="document.location.href='../../LoginUnregisteredPatient/LoginPage/logout.php'">Logout</button>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- content -->
+    <div class="container my-5">
+        <h1 class="pt-3">Passwords don't match! please press the button to try again</h5>
+        <div class="text-center"><button class="btn btn-dark mt-4 px-5" onclick="window.location.href='./changeUsernameOrPassword.php';">Return</button></div>
+    </div>
+
+    <!-- bootstrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
+
+    <!-- bootstrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
