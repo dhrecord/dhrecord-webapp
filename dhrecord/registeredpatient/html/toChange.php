cf29402e093e1795da928c2b31239ffab1591cc1
dhrecord <dentalrecord00@gmail.com>
2022-08-02 16:27:28 +0800

    .

---
+++ dhrecord/registeredpatient/html/toChange.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['loggedin']))
+{
+	header('Location: ../../LoginUnregisteredPatient/LoginPage/index.html');
+	exit;
+}
+
+$userID = $_POST['userID'];
+$userName = $_POST['userName'];
+$newPassWord = $_POST['newPassWord'];
+$confirmNewPassWord = $_POST['confirmNewPassWord'];
+
+//Database Connection
+$servername = "localhost";
+$database = "u922342007_Test";
+$username = "u922342007_admin";
+$password = "Aylm@012";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) 
+{
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+if($newPassWord = $confirmNewPassWord)
+{
+	$query = "UPDATE users SET username='$userName', password='$newPassWord' WHERE ID='$userID'";
+}
+else 
+{
+	echo "passwords do not match!";
+	header(location: changeUsernameOrPassword.php);
+}
+
+
+?>
