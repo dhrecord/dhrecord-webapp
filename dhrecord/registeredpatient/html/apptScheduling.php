92a67d66844e530af7dbc6b42716014332fe39ca
Aurellya <aurellyagho25@gmail.com>
2022-08-02 22:22:09 +0800

    [formatting]

--- dhrecord/registeredpatient/html/apptScheduling.php
+++ dhrecord/registeredpatient/html/apptScheduling.php
@@ -318,7 +318,7 @@
       <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
           <div class="modal-content">
               <div class="modal-header">
-                  <h5 class="modal-title" id="popupModalLabel">Operating Hours</h5>
+                  <h5 class="modal-title" id="popupModalLabel">Information</h5>
                   <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
@@ -326,7 +326,7 @@
                 <br/>
                 <b>Services:</b> -
                 <br/>
-                <b>Operating Hours: -</b>
+                <b>Operating Hours: </b> -
                 </p>
               </div>
           </div>
