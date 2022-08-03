b0cd9e9de7a0421077635d629c4ece237c25bd4c
dhrecord <dentalrecord00@gmail.com>
2022-08-03 20:16:53 +0800

    .

--- dhrecord/businessowner/html/manageRecord.php
+++ dhrecord/businessowner/html/manageRecord.php
@@ -150,11 +150,10 @@
                     <td><button class="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#popupModal<?php echo $row["ID"]; ?>">Edit</button></td>
                     <td><button type="button" class="btn btn-sm btn-success" onclick="document.location.href='deleteUser.php?UserID=<?php echo $row["userID"]; ?>'">Delete</button></td>
                </tr>
-            </tbody>
-        </table>
+
 
         <!-- modal -->
-        <div class="modal fade" id="popupModal<?php echo $row["clinicadminID"]; ?>" tabindex="-1" aria-labelledby="popupModalLabel" aria-hidden="true">
+        <div class="modal fade" id="popupModal<?php echo $row["ID"]; ?>" tabindex="-1" aria-labelledby="popupModalLabel" aria-hidden="true">
             <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
                 <div class="modal-content">
                     <div class="modal-header">
@@ -162,15 +161,19 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <form action="./editUserClinicAdmin.php" method="post">
+                        <form action="./editUserPatient.php" method="post">
                             <p style="display: none;" id="invisibleID"></p>
                             <div class="mb-3">
-                                <label for="clinicadminID" class="form-label">Clinic Admin ID</label>
-                                <input type="text" class="form-control" id="clinicadminID" name="clinicadminID" <?php echo 'value="'.$row["clinicadminID"].'"'; ?> readonly>
+                                <label for="clinicadminID" class="form-label">Patient ID</label>
+                                <input type="text" class="form-control" id="patientID" name="patientID" <?php echo 'value="'.$row["ID"].'"'; ?> readonly>
                             </div>
                             <div class="mb-3">
                                 <label for="fullName" class="form-label">Full Name</label>
-                                <input type="text" class="form-control" id="fullName" name="fullName" <?php echo 'value="'.$row["fullName"].'"'; ?>>
+                                <input type="text" class="form-control" id="fullName" name="fullName" <?php echo 'value="'.$row["fullName"].'"'; ?> readonly>
+                            </div>
+                            <div class="mb-3">
+                                <label for="nricNumber" class="form-label">NRIC Number</label>
+                                <input type="text" class="form-control" id="nricNumber" name="nricNumber" <?php echo 'value="'.$row["nricNumber"].'"'; ?> readonly>
                             </div>
                             <div class="mb-3">
                                 <label for="contactNumber" class="form-label">Contact Number</label>
@@ -181,8 +184,12 @@
                                 <input type="text" class="form-control" id="email" name="email" <?php echo 'value="'.$row["email"].'"'; ?>>
                             </div>
                             <div class="mb-3">
-                                <label for="clinicID" class="form-label">Clinic ID</label>
-                                <input type="text" class="form-control" id="clinicID" name="clinicID"<?php echo 'value="'.$row["clinicID"].'"'; ?> readonly>
+                                <label for="medConditions" class="form-label">Medical Conditions</label>
+                                <input type="text" class="form-control" id="medConditions" name="medConditions" <?php echo 'value="'.$row["medConditions"].'"'; ?>>
+                            </div>
+                                                        <div class="mb-3">
+                                <label for="drugAllergies" class="form-label">Drug Allergies</label>
+                                <input type="text" class="form-control" id="drugAllergies" name="drugAllergies" <?php echo 'value="'.$row["drugAllergies"].'"'; ?>>
                             </div>
                             <div class="mb-3 row">
                                 <div class="text-center"><button type="submit" class="btn btn-dark mt-4 px-5">Submit</button></div>
@@ -197,6 +204,9 @@
             }
         }
         ?>
+
+                    </tbody>
+        </table>
 
 
     <!-- bootstrap js -->
