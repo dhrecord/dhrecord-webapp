bd055e9ddee18c0a5d8740ae590705a2524fcc28
dhrecord <dentalrecord00@gmail.com>
2022-08-03 21:53:08 +0800

    .

---
+++ dhrecord/businessowner/html/deletePatient.php
@@ -0,0 +1,46 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['loggedin']))
+{
+	header('Location: ../../LoginUnregisteredPatient/LoginPage/index.html');
+	exit;
+}
+
+$UserID = $_GET['UserID'];
+	
+//Database Connection
+$servername = "localhost";
+$database = "u922342007_Test";
+$username = "u922342007_admin";
+$password = "Aylm@012";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) 
+{
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+$query = "SELECT * FROM users WHERE ID='$UserID'";
+$result = $conn->query($query);
+$row = $result->fetch_assoc();
+
+$queryDelete1 = "DELETE FROM registeredPatient WHERE users_ID='$UserID'"; 
+$queryDelete2 = "DELETE FROM users WHERE ID='$UserID'"; 
+	
+if (mysqli_query($conn,$queryDelete1) && mysqli_query($conn,$queryDelete2))
+{
+	mysqli_close();
+	header('Location: userManagement.php');
+}
+
+else
+{
+	echo "something went wrong!";
+	mysqli_close();
+}
+
+
+?>
