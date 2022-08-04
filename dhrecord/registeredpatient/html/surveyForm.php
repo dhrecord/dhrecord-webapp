7d0ac103eca5496f1c757bbc2d9a28e15a147ada
vanchio <108721199+vanchio@users.noreply.github.com>
2022-08-04 20:21:58 +0800

    Update surveyForm.php

--- dhrecord/registeredpatient/html/surveyForm.php
+++ dhrecord/registeredpatient/html/surveyForm.php
@@ -9,6 +9,8 @@
 
   $uName=$_SESSION['username'];
   $estimatedWait=$_POST['estimatedWait'];
+	$clinicRef=$_POST['çlinicRef'];
+	$docRef=$_POST['docRef'];
   $serviceQuality=$_POST['serviceQuality'];
   $recommended=$_POST['recommended'];
   $medconds=$_POST['medconds'];
@@ -20,8 +22,8 @@
 
   $conn = mysqli_connect($servername, $username, $password, $database);
 
-  $stmt = mysqli_prepare($conn, "insert into `surveyForm`(`username`,`timeTaken`, `rating`, `recommendation`,`remarks`) values (?, ?, ?, ?, ?)");
-	mysqli_stmt_bind_param($stmt, "sssss", $uName, $estimatedWait, $serviceQuality, $recommended, $medconds);
+  $stmt = mysqli_prepare($conn, "insert into `surveyForm`(`username`,`timeTaken`, `nameClinic` , `nameDoc` , `rating` , `recommendation`,`remarks`) values (?, ?, ?, ?, ?, ?, ?)");
+	mysqli_stmt_bind_param($stmt, "sssssss", $uName, $estimatedWait, $clinicRef, $docRef, $serviceQuality, $recommended, $medconds);
  //$stmt = mysqli_prepare($conn, "insert into `surveyForm`(`timeTaken`, `rating`, `recommendation`,`remarks`) values (?, ?, ?, ?)");
 	//mysqli_stmt_bind_param($stmt, "ssis", $estimatedWait, $serviceQuality, $recommended, $medconds);
 	mysqli_stmt_execute($stmt);
