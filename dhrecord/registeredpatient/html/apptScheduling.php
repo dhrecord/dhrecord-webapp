b4f39f61984120844d517e9b6364d4c38660e065
Aurellya <aurellyagho25@gmail.com>
2022-08-05 03:02:47 +0800

    [...]

--- dhrecord/registeredpatient/html/apptScheduling.php
+++ dhrecord/registeredpatient/html/apptScheduling.php
@@ -25,6 +25,7 @@
   if(isset($_POST['save'])){
     if(!empty($_POST['search'])){
       $search = $_POST['search'];
+      $search = "%$search%"; // prepare the $search variable
       $stmt = $conn->prepare("SELECT * FROM businessOwner WHERE nameOfClinic LIKE ?");
       $stmt->bind_param("s", $search);
       $stmt->execute();
