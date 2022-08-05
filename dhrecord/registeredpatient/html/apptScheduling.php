2bfe2b9413647964c6d6f6eb423cf9be3f900efb
Aurellya <aurellyagho25@gmail.com>
2022-08-06 03:40:04 +0800

    [add star icon for rating]

--- dhrecord/registeredpatient/html/apptScheduling.php
+++ dhrecord/registeredpatient/html/apptScheduling.php
@@ -277,7 +277,12 @@
 
                     $field5 = $row['rating'];
                     if ($field5){
-                      echo $field5; 
+                      echo '<div class="d-flex">';
+                      $field5 = number_format($field5);
+                      for ($x = 0; $x <= $field5; $x++) {
+                        echo '<i class="fa-solid fa-star"></i>';
+                      }
+                      echo '</div>';
                     } else{
                       echo '-';
                     }
