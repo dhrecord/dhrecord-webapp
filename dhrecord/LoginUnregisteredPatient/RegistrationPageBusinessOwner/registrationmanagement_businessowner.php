35bd83278c9d7379aa853310c0479778f382fc4f
dhrecord <dentalrecord00@gmail.com>
2022-08-05 15:20:13 +0800

    [updated navbar]

--- dhrecord/LoginUnregisteredPatient/RegistrationPageBusinessOwner/registrationmanagement_businessowner.php
+++ dhrecord/LoginUnregisteredPatient/RegistrationPageBusinessOwner/registrationmanagement_businessowner.php
@@ -18,44 +18,28 @@
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid container">
-            <a class="navbar-brand" href="./home.html"><b>DHRecord</b></a>
+            <a class="navbar-brand" href="../ClinicSearch/index.php"><b>DHRecord</b></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
                 aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <!--<div class="collapse navbar-collapse" id="navbarText">
+            <div class="collapse navbar-collapse" id="navbarText">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="./home.html">Home</a>
+                        <a class="nav-link" aria-current="page" href="../ClinicSearch/index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="./auditlog.html">Audit Log</a>
+                        <a class="nav-link" aria-current="page" href="../About/index.html">About</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="./usermanagement.html">User Management</a>
+                        <a class="nav-link" aria-current="page" href="../ContactUs/index.html">Contact Us</a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle active" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Registration Management
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="./registrationmanagement_businessowner.html">Business
-                                    Owner</a></li>
-                            <li><a class="dropdown-item" href="./registrationmanagement_patientaccount.html">Patient
-                                    Account</a></li>
-                        </ul>
-                    </li>
-
                 </ul>
-                <div class="d-flex flex-column align-items-end">
-                    <p class="navbar-text text-white m-0">
-                        Welcome, Username
-                    </p>
-                    <button type="button" class="btn btn-light ml-3 btn-sm mb-2" style="width: 90px;"
-                        onclick="document.location.href='./login.html'">Logout</button>
+                <div>
+                    <button type="button" class="btn btn-light btn-sm" style="width: 90px;"
+                        onclick="window.location.href='../LoginPage/index.html'">Login</button>
                 </div>
-            </div>-->
+            </div>
         </div>
     </nav>
 
