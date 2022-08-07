ea5714dfdf5c97d776687dd8a8937394a53c1c05
Aurellya <aurellyagho25@gmail.com>
2022-08-07 23:02:23 +0800

    [...]

--- dhrecord/registeredpatient/html/bookApptSubmit.php
+++ dhrecord/registeredpatient/html/bookApptSubmit.php
@@ -9,11 +9,11 @@
   $docID = $_POST['docID'];
   $patID = $_POST['patID'];
   $agenda = '"'.$_POST['agenda'].'"';
-  $date = '"'.$_POST['date'].'"';
+  $date = $_POST['date'];
   $time = $_POST['time'];
 
   // convert date
-  $formattedDate = substr($date, 6, 4)."-".substr($date, 0, 2)."-".substr($date, 3, 2);
+  $formattedDate = '"'.substr($date, 6, 4)."-".substr($date, 0, 2)."-".substr($date, 3, 2).'"';
 
   $servername = "localhost";
   $database = "u922342007_Test";
@@ -26,13 +26,13 @@
   if(count($timeArray)<= 2){
     $time = '"'.substr($time, 0, 5).':00'.'"';
     $stmt = mysqli_prepare($conn, "insert into `appointment`(`date`, `time`, `agenda` , `doctorID` , `patientID`) values (?, ?, ?, ?, ?");
-    mysqli_stmt_bind_param($stmt, "sssss", $date, $time, $agenda, $docID, $patID);
+    mysqli_stmt_bind_param($stmt, "sssss", $formattedDatedate, $time, $agenda, $docID, $patID);
     mysqli_stmt_execute($stmt);
   } else {
     for($i=0;$i<count($timeArray);$i++){
         $timeBlock = '"'.$timeArray[$i].":00".'"';
         $stmt = mysqli_prepare($conn, "insert into `appointment`(`date`, `time`, `agenda` , `doctorID` , `patientID`) values (?, ?, ?, ?, ?");
-        mysqli_stmt_bind_param($stmt, "sssss", $date, $timeBlock, $agenda, $docID, $patID);
+        mysqli_stmt_bind_param($stmt, "sssss", $formattedDate, $timeBlock, $agenda, $docID, $patID);
         mysqli_stmt_execute($stmt);
     }
   }
