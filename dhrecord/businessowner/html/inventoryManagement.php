916b74d62ceadf2a6d653da8d1f92a2205344630
jovencsw <jovencsw@gmail.com>
2022-08-08 16:54:03 +0800

    test 3

--- dhrecord/businessowner/html/inventoryManagement.php
+++ dhrecord/businessowner/html/inventoryManagement.php
@@ -175,7 +175,7 @@
                     {
                         $searchkey= $_POST['search'];
                         $res = mysqli_query($conn, "SELECT * FROM `inventoryManagement` WHERE prescriptionName LIKE '%$searchkey%'");
-                            
+                            //test 1
                     }
 
                 else 
