49439ea786920ffbfe249583d8870f617b94ec06
dhrecord <dentalrecord00@gmail.com>
2022-08-09 01:32:48 +0800

    .

--- dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/connect.php
+++ dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/connect.php
@@ -32,19 +32,30 @@
 		die("Connection failed: " . mysqli_connect_error());
 	}
 
-	$subject = "Email Verification";
-	$message = "Hi! Please click the link below to verify your email!<br> <a href='http://dhrecord.com/dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/verify.php?vkey=$vkey'>Register Account</a>";
-	$headers = "From: dentalrecord00@gmail.com \r\n";
-	$headers .= "MIME-version: 1.0" . "\r\n";
-	$headers .= "content-type:text/html;charset=UTF-8" . "\r\n";
+	$name = test_input($_POST['fullName']);
 
-	mail($email,$subject,$message,$headers);
+	if(!preg_match("/^[a-zA-Z-' ]*$/",$name))
+	{
 	
-	$stmt = mysqli_prepare($conn, "insert into tempRegisteredPatient(fullName, nricNumber, contactNumber, email, address, medConditions, drugAllergies, role, username, password, vkey, verified) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-	mysqli_stmt_bind_param($stmt, "sssssssssssi", $fullName, $nricNumber, $contactNumber, $email, $address, $medConditions, $drugAllergies, $role, $userName, $passWord, $vkey, $verifiedNegative);
-	mysqli_stmt_execute($stmt);
+		$nameErr = "Only letters and white space allowed";
+	
+	}
+	else
+	{
+		$subject = "Email Verification";
+		$message = "Hi! Please click the link below to verify your email!<br> <a href='http://dhrecord.com/dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/verify.php?vkey=$vkey'>Register Account</a>";
+		$headers = "From: dentalrecord00@gmail.com \r\n";
+		$headers .= "MIME-version: 1.0" . "\r\n";
+		$headers .= "content-type:text/html;charset=UTF-8" . "\r\n";
 
-	mysqli_close($conn);
-	header("Location: http://dhrecord.com/dhrecord/LoginUnregisteredPatient/LoginPage/");
+		mail($email,$subject,$message,$headers);
+	
+		$stmt = mysqli_prepare($conn, "insert into tempRegisteredPatient(fullName, nricNumber, contactNumber, email, address, medConditions, drugAllergies, role, username, password, vkey, verified) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		mysqli_stmt_bind_param($stmt, "sssssssssssi", $fullName, $nricNumber, $contactNumber, $email, $address, $medConditions, $drugAllergies, $role, $userName, $passWord, $vkey, $verifiedNegative);
+		mysqli_stmt_execute($stmt);
+
+		mysqli_close($conn);
+		header("Location: http://dhrecord.com/dhrecord/LoginUnregisteredPatient/LoginPage/");
+	}
 
 ?>
