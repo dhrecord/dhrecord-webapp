cf340d27ffcdf41cb6a17cd53a0be6677ae0eb4c
dhrecord <dentalrecord00@gmail.com>
2022-08-09 17:25:56 +0800

    .

--- dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/connect.php
+++ dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/connect.php
@@ -70,7 +70,7 @@
 		mail($email,$subject,$message,$headers);
 	
 		$stmt = mysqli_prepare($conn, "insert into tempRegisteredPatient(fullName, nricNumber, contactNumber, email, address, medConditions, drugAllergies, role, username, password, attendingDoctor, vkey, verified) values (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-		mysqli_stmt_bind_param($stmt, "sssssssssssi", $fullName, $nricNumber, $contactNumber, $email, $address, $medConditions, $drugAllergies, $role, $userName, $passWord, $attendingDoctor, $vkey, $verifiedNegative);
+		mysqli_stmt_bind_param($stmt, "ssssssssssssi", $fullName, $nricNumber, $contactNumber, $email, $address, $medConditions, $drugAllergies, $role, $userName, $passWord, $attendingDoctor, $vkey, $verifiedNegative);
 		mysqli_stmt_execute($stmt);
 
 		mysqli_close($conn);
