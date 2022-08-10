f1e8bf3422b942e118c3440991a1a6c39b5794e3
xgladysx <92320525+xgladysx@users.noreply.github.com>
2022-08-10 17:05:33 +0800

    navbar

--- dhrecord/businessowner/html/addUser.php
+++ dhrecord/businessowner/html/addUser.php
@@ -47,94 +47,9 @@
 </head>
 
 <body>
-    <!-- navbar -->
-     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid container">
-            <?php 
-		$role = $_SESSION['role'];
-		if ($role === 'dr')
-		{
-			echo '<a class="navbar-brand" href="./drindex.php"><b>DHRecord</b></a>';
-		} else if ($role === "fd")
-		{
-			echo '<a class="navbar-brand" href="./fdindex.php"><b>DHRecord</b></a>';
-		} else
-		{
-			echo '<a class="navbar-brand" href="./caindex.php"><b>DHRecord</b></a>';
-		}
-	   ?>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
-                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <?php 
-				$role = $_SESSION['role'];
-				if ($role === 'dr')
-				{
-					echo '<a class="nav-link active" aria-current="page" href="./drindex.php">Home</a>';
-				} else if ($role === "fd")
-				{
-					echo '<a class="nav-link active" aria-current="page" href="./fdindex.php">Home</a>';
-				} else
-				{
-					echo '<a class="nav-link active" aria-current="page" href="./caindex.php">Home</a>';
-				}
-			?>
-                    </li>
-                    <!--<li class="nav-item">
-                        <a class="nav-link" href="./userManagement.php">User Management</a>
-                    </li>-->
-                    <!--<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="nav-link" href="./userManagement.php">User Management</a></li>
-                        <li><a class="dropdown-item" href="./manageRecord.php">View Patient</a></li>
-                    </ul>-->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            User & Records
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="./userManagement.php">User Management</a></li>
-                            <li><a class="dropdown-item" href="./manageRecord.php">View Patient</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./referralTracking.php">Referral Tracking</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            Appointment & Treatment
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="./apptSchedulingAndReminders.php">Appointment Scheduling
-                                    & Reminders</a></li>
-                            <li><a class="dropdown-item" href="./treatmentHistory.php">Treatment Planning</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./reportingAndStatistics.php">Reporting & Statistics</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./billingInvoicing.php">Payment</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="./inventoryManagement.php">Inventory Management</a>
-                    </li>
-                </ul>
-                <div class="d-flex flex-column align-items-end">
-                    <p class="navbar-text text-white m-0">
-                        Welcome, <?php echo $_SESSION['username']; ?>
-                    </p>
-                    <button type="button" class="btn btn-light ml-3 btn-sm mb-2"
-                        onclick="document.location.href='../../LoginUnregisteredPatient/LoginPage/logout.php'">Logout</button>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php
+    include 'navBar.php';
+?>
 
     <!-- content -->
     <div class="container my-5">
