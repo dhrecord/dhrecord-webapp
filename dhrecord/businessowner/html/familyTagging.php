46016954171b0c4217ec6ca36feffd3a7cd4459a
dhrecord <dentalrecord00@gmail.com>
2022-08-10 15:48:04 +0800

    .

---
+++ dhrecord/businessowner/html/familyTagging.php
@@ -0,0 +1,189 @@
+<?php
+
+session_start();
+if(!isset($_SESSION['loggedin']))
+{
+	header('Location: ../../LoginUnregisteredPatient/LoginPage/index.html');
+	exit;
+}
+
+$UserID = $_GET['UserID'];
+	
+//Database Connection
+$servername = "localhost";
+$database = "u922342007_Test";
+$username = "u922342007_admin";
+$password = "Aylm@012";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+
+if (!$conn) 
+{
+	die("Connection failed: " . mysqli_connect_error());
+}
+
+$query = "SELECT * FROM registeredPatient WHERE users_ID='$UserID'";
+$result = $conn->query($query);
+$row = $result->fetch_assoc();
+
+$patientID = $row["ID"];
+$fullName = $row["fullName"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>DHRecord</title>
+    <link rel="stylesheet" href="../css/style.css" />
+
+    <!-- bootstrap css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <!-- navbar -->
+     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid container">
+            <?php 
+		$role = $_SESSION['role'];
+		if ($role === 'dr')
+		{
+			echo '<a class="navbar-brand" href="./drindex.php"><b>DHRecord</b></a>';
+		} else if ($role === "fd")
+		{
+			echo '<a class="navbar-brand" href="./fdindex.php"><b>DHRecord</b></a>';
+		} else
+		{
+			echo '<a class="navbar-brand" href="./caindex.php"><b>DHRecord</b></a>';
+		}
+	   ?>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <?php 
+				$role = $_SESSION['role'];
+				if ($role === 'dr')
+				{
+					echo '<a class="nav-link active" aria-current="page" href="./drindex.php">Home</a>';
+				} else if ($role === "fd")
+				{
+					echo '<a class="nav-link active" aria-current="page" href="./fdindex.php">Home</a>';
+				} else
+				{
+					echo '<a class="nav-link active" aria-current="page" href="./caindex.php">Home</a>';
+				}
+			?>
+                    </li>
+                    <!--<li class="nav-item">
+                        <a class="nav-link" href="./userManagement.php">User Management</a>
+                    </li>-->
+                    <!--<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="nav-link" href="./userManagement.php">User Management</a></li>
+                        <li><a class="dropdown-item" href="./manageRecord.php">View Patient</a></li>
+                    </ul>-->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            User & Records
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="./userManagement.php">User Management</a></li>
+                            <li><a class="dropdown-item" href="./manageRecord.php">View Patient</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./referralTracking.php">Referral Tracking</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-bs-toggle="dropdown" aria-expanded="false">
+                            Appointment & Treatment
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="./apptSchedulingAndReminders.php">Appointment Scheduling
+                                    & Reminders</a></li>
+                            <li><a class="dropdown-item" href="./treatmentHistory.php">Treatment Planning</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./reportingAndStatistics.php">Reporting & Statistics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./billingInvoicing.php">Payment</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./inventoryManagement.php">Inventory Management</a>
+                    </li>
+                </ul>
+                <div class="d-flex flex-column align-items-end">
+                    <p class="navbar-text text-white m-0">
+                        Welcome, <?php echo $_SESSION['username']; ?>
+                    </p>
+                    <button type="button" class="btn btn-light ml-3 btn-sm mb-2"
+                        onclick="document.location.href='../../LoginUnregisteredPatient/LoginPage/logout.php'">Logout</button>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- content -->
+    <div class="container my-5">
+        <h4 class="mb-4">Add New User</h4>
+        <form method="post" action="./tagNewFamily.php" method="POST">
+            <div class="container border border-dark p-4" style="border-top-width: 10px!important;">
+                <div class="mb-3 row">
+                    <label for="patientID" class="col-sm-2 col-form-label">Patient ID</label>
+                    <div class="col-sm-10">
+                        <input type="number" class="form-control" id="patientID" name="patientID" <?php echo 'value = "'.$patientID. '"'; ?> readonly> 
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="fullName" class="col-sm-2 col-form-label">Full Name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="fullName" name="fullName" <?php echo 'value = "'.$fullName. '"'; ?> readonly>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <label for="familyMember" class="col-sm-2 col-form-label">Family Member</label>
+                    <div class="col-sm-10">
+                        <?php
+                        $sql1 = "SELECT * FROM `registeredPatient`";
+                        $result1 = mysqli_query($conn,$sql1);
+
+                        echo "<select name='familyMember'>";
+                        
+                        while($row1 = mysqli_fetch_array($result1))
+                        {
+                            echo "<option value='" . $row1['ID'] ."'>" . $row1['fullName'] ."</option>";
+                        }
+
+                        echo "</select>";
+                        ?>
+                    </div>
+                </div>
+                <div class="mb-3 row">
+                    <div class="text-center"><button type="submit" class="btn btn-dark mt-4 px-5">Submit</button></div>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!-- bootstrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
--- dhrecord/businessowner/html/manageRecord.php
+++ dhrecord/businessowner/html/manageRecord.php
@@ -102,6 +102,7 @@
                     <td><?php echo $row["email"]; ?></td>
                     <td><button class="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#popupModal<?php echo $row["ID"]; ?>">Edit</button></td>
                     <td><button type="button" class="btn btn-sm btn-success" onclick="document.location.href='deletePatient.php?UserID=<?php echo $row["users_ID"]; ?>'">Delete</button></td>
+                    <td><button type="button" class="btn btn-sm btn-success" onclick="document.location.href='familyTagging.php?UserID=<?php echo $row["users_ID"]; ?>'">Family Tagging</button></td>
                </tr>
 
 
