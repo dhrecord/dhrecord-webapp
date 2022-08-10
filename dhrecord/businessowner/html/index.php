8e6fc009cf58137532964b44faeb03ce6d7db00e
Aurellya <aurellyagho25@gmail.com>
2022-08-11 00:31:47 +0800

    [\-/-\]

--- dhrecord/businessowner/html/index.php
+++ dhrecord/businessowner/html/index.php
@@ -7,6 +7,25 @@
         exit;
     }
 
+    // link in navbar active -> white color when selected
+    function includeWithVariables($filePath, $variables = array(), $print = true)
+    {
+        $output = NULL;
+        if(file_exists($filePath)){
+            // Extract the variables to a local namespace
+            extract($variables);
+            // Start output buffering
+            ob_start();
+            // Include the template file
+            include $filePath;
+            // End buffering and return its contents
+            $output = ob_get_clean();
+        }
+        if ($print) {
+            print $output;
+        }
+        return $output;
+    }
 ?>
 
 
@@ -28,7 +47,8 @@
 
 <body>
 <?php
-    include 'navBar.php';
+    // include 'navBar.php';
+    includeWithVariables('navBar.php', array('title' => 'Home'));
 ?>
 
 
