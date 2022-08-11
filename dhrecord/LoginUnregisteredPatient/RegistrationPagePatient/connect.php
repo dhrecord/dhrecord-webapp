4b948bf9ce15f0142b0f39081d43da1667ad3043
dhrecord <dentalrecord00@gmail.com>
2022-08-11 20:05:37 +0800

    .

--- dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/connect.php
+++ dhrecord/LoginUnregisteredPatient/RegistrationPagePatient/connect.php
@@ -15,6 +15,21 @@
 	$userName = $_POST['userName'];
 	$passWord = $_POST['passWord'];
 
+	// Store the cipher method
+	$ciphering = "AES-128-CTR";
+  
+	// Use OpenSSl Encryption method
+	$iv_length = openssl_cipher_iv_length($ciphering);
+	$options = 0;
+
+	// Non-NULL Initialization Vector for encryption
+	$encryption_iv = '1234567891011121';
+
+	// Store the encryption key
+	$encryption_key = "JovenChanDunCry";
+
+	$encryptedPassword = openssl_encrypt($simple_string, $ciphering, $encryption_key, $options, $encryption_iv);
+
 	//timestamp
 	$vkey = md5(time().$userName);
 	$verifiedNegative = 0;
