1e2a212b274a333784e7696298bde9f6bd6314fe
Aurellya <aurellyagho25@gmail.com>
2022-08-16 19:22:28 +0800

    [-]

--- dhrecord/superadmin/html/registrationmanagement_businessowner.php
+++ dhrecord/superadmin/html/registrationmanagement_businessowner.php
@@ -7,6 +7,20 @@
     exit;
     }
 
+    //Database Connection
+    $servername = "localhost";
+    $database = "u922342007_Test";
+    $username = "u922342007_admin";
+    $password = "Aylm@012";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    if (!$conn) 
+    {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
     $result = '';
     $search = '';
     $select = '';
@@ -226,20 +240,7 @@
             </thead>
             <tbody>
                 <?php
-	            //Database Connection
-	            $servername = "localhost";
-	            $database = "u922342007_Test";
-	            $username = "u922342007_admin";
-	            $password = "Aylm@012";
-
-	            // Create connection
-	            $conn = mysqli_connect($servername, $username, $password, $database);
-
-	            if (!$conn) 
-	            {
-		            die("Connection failed: " . mysqli_connect_error());
-	            }
-                
+	            
                 //$stmt = $conn->prepare("SELECT * FROM businessOwnerForApproval");
 		        //$stmt->execute();
 		        //$stmt_result = $stmt->get_result();
