96946e185b9f281795241d37007f2eefe44e70e9
Aurellya <aurellyagho25@gmail.com>
2022-08-20 02:55:24 +0800

    [-]

--- dhrecord/LoginUnregisteredPatient/ClinicSearch/index.php
+++ dhrecord/LoginUnregisteredPatient/ClinicSearch/index.php
@@ -201,11 +201,11 @@
 
     <div class="container my-5 custom-container">
         <div class="mb-5 filter-div">
-            <div class="d-flex align-items-center">
+            <div class="search-clinic-div">
                 <div><p class="m-0"><b>Search Clinic:</b></p></div>
 
-                <form action="#" method="post" class="d-flex">
-                  <div class="input-group mx-4" style="width:fit-content">
+                <form action="#" method="post" class="d-flex search-clinic-form">
+                  <div class="input-group mx-4" style="width:fit-content; flex-wrap: no-wrap;">
                     <input type="text" id="searchInput" class="form-control" name="search" placeholder="Enter Value ..."
                     aria-label="search" aria-describedby="basic-addon2" style="max-width: 350px; min-width: 250px;" required/>
                     <button id="basic-addon2" type="submit" name="save" class="input-group-text">
@@ -213,7 +213,7 @@
                     </button>
                   </div>
 
-                    <div class="mx-2"> 
+                    <div class="select-div1"> 
                       <select required name="select" class="form-select" id="auditLog_ddlFilterBy" aria-label="Filter By..."
                     style="" data-bs-toggle="tooltip" data-bs-placement="top" title="Time Format E.g. 16:00 ">
                         <option value="" selected disabled hidden>Category ...</option>
