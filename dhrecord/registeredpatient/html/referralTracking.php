cd35f2798cbf4c1de9a22a79ff3d90f13c93d7bc
Aurellya <aurellyagho25@gmail.com>
2022-08-20 14:40:47 +0800

    [-]

--- dhrecord/registeredpatient/html/referralTracking.php
+++ dhrecord/registeredpatient/html/referralTracking.php
@@ -16,7 +16,7 @@ if(!isset($_SESSION['loggedin']))
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>DHRecord</title>
-    <link rel="stylesheet" href="../css/style.css" />
+    <link rel="stylesheet" href="../css/apptSched.css" />
 
     <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
