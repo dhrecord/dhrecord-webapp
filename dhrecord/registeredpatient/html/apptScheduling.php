d3d39c3907b54f7894ffe0ede62069c1f02f0622
Aurellya <aurellyagho25@gmail.com>
2022-08-20 14:18:20 +0800

    [finished responsive => reg pt appt sched]

--- dhrecord/registeredpatient/html/apptScheduling.php
+++ dhrecord/registeredpatient/html/apptScheduling.php
@@ -175,7 +175,7 @@
 
   <!-- Style -->
   <link rel="stylesheet" href="../../apptScheduling/css/style.css">
-  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/apptSched.css">
 
   <!-- fontawesome -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
